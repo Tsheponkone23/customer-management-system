dc20c342e0961db0216d6dec9e9e4f0620055aec
Tshepo Nkone <tshepo.nkone@standardbank.co.za>
2021-02-18 10:28:39 +0200

    first commit

---
+++ src/test/java/com/customermanagementsystem/customermanagementsystem/CustomerManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customermanagementsystem.customermanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
